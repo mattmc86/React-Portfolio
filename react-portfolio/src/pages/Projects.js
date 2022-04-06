f8ef91de6251c99d2b5f0c9491a16b5043c1a485
Matt <86723332+mattmc86@users.noreply.github.com>
2022-04-06 22:17:13 +0100

    Added navbar and pages to navigate to

---
+++ react-portfolio/src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>
+      TODO - put projects here
+      </p>
+    </div>
+  );
+}
